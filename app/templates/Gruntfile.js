1e7e9a9a583cef9143399584116264fcb77e5d93
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-07-28 10:41:19 +0200

    Add webpack configuration

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,9 +1,46 @@
+var BannerPlugin = require('webpack/lib/BannerPlugin');
+
 module.exports = function(grunt) {
 
     // Project configuration.
-    var banner = '/*! <%= pkg.name %> v<%= pkg.version %> */\n';
+    var pkg = grunt.file.readJSON('package.json');
+    var licenses = '';
+    (pkg.licenses || []).forEach(function(l) {
+        if (licenses.length) {
+            licenses += ', ';
+        }
+        licenses += l ? l.type || '' : '';
+    });
+    if (licenses.length) {
+        licenses = ' | License: ' + licenses + ' ';
+    }
+    var banner = '<%= pkg.name %> v<%= pkg.version %>' + licenses;
     grunt.initConfig({
-        pkg : grunt.file.readJSON('package.json'),
+        pkg : pkg,
+        webpack : {
+            main : {
+                entry : './src/index',
+                output : {
+                    path : './dist',
+                    filename : pkg.name + '.js',
+                    library : pkg.name,
+                    libraryTarget : 'umd',
+                    sourcePrefix : ''
+                },
+                plugins : [ new BannerPlugin(banner) ]
+            },
+            minified : {
+                entry : './src/index',
+                output : {
+                    // optimize-minimize option
+                    path : './dist',
+                    filename : pkg.name + '.min.js',
+                    library : pkg.name,
+                    libraryTarget : 'umd',
+                    sourcePrefix : ''
+                }
+            }
+        },
         bump : {
             options : {
                 files : [ 'package.json', 'bower.json' ],
@@ -29,11 +66,11 @@ module.exports = function(grunt) {
         },
         uglify : {
             options : {
-                banner : banner
+                banner : '/*! ' + banner + ' */\n'
             },
             dest : {
-                src : 'src/**/*.js',
-                dest : 'dist/<%= pkg.name %>.min.js'
+                src : './dist/' + pkg.name + '.min.js',
+                dest : './dist/' + pkg.name + '.min.js'
             }
         },
         jshint : {
@@ -54,13 +91,18 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-webpack');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-bump');
 
     // this would be run by typing "grunt test" on the command line
     grunt.registerTask('test', [ 'jshint', 'mochaTest' ]);
 
+    grunt.registerTask('build', [ 'jshint', 'mochaTest', 'webpack:main' ]);
+    grunt.registerTask('build-min', [ 'jshint', 'mochaTest',
+            'webpack:minified', 'uglify' ]);
+
     // Default task(s).
     // the default task can be run just by typing "grunt" on the command line
-    grunt.registerTask('default', [ 'jshint', 'mochaTest', 'uglify' ]);
+    grunt.registerTask('default', [ 'build', 'build-min' ]);
 }
